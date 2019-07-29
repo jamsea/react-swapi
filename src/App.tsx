82f7536bcb6270c715642ff914d732da785c9084
James Hush <jhush91@gmail.com>
2019-07-28 18:49:29 -0700

    Add film times and preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import "./App.css";
+
+import { Film } from "./films.d";
+
+// https://swapi.co/api/films/
 
 const App: React.FC = () => {
+  const [data, setData] = useState("");
+  const [query, setQuery] = useState("redux");
+  const [url, setUrl] = useState("https://swapi.co/api/films/?search=New");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      const res = await fetch(url);
+      const jsonRes = await res.json();
+      console.log("----jsonRes: ", jsonRes);
+      const results = jsonRes.results as Film[];
+      setData(jsonRes.results[0].title);
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header" />
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() => setUrl(`https://swapi.co/api/films/?search=${query}`)}
+      >
+        Search
+      </button>
+
+      {isLoading ? <div>Loading ...</div> : <h1>{data}</h1>}
     </div>
   );
-}
+};
 
 export default App;
